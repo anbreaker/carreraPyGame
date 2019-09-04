416bdcb5412de44bef6bf2de305a6e560c00c8b8
anbreaker <antunez19@gmail.com>
2019-09-04 22:58:28 +0200

    Bola andando y cortando el programa al llegar al final

--- mainPyGame.py
+++ mainPyGame.py
@@ -18,15 +18,19 @@ class Game():
         #Cargamos la imagen de fondo del display
         self.background = pygame.image.load("images/background.png")
 
+        #cargo runnerBola
+        self.runner = pygame.image.load("images/smallBall1.png")
+
     def competir(self):
         pygame.font.init()
         pygame.init()
         self.__screen = pygame.display.set_mode(self.__size, pygame.HWSURFACE)
         pygame.display.set_caption(self.__title)
         
-        ganador = False
+        hayGanador = False
+        x = 0        
 
-        while not ganador:
+        while not hayGanador:
             #Comprobacion de los eventos
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -36,11 +40,22 @@ class Game():
             #Renderizado de la pantalla
             #Nos pinta la pantalla con el background
             self.__screen.blit(self.background, (0,0))
+            self.__screen.blit(self.runner, (x,240))
+            
             #refresco de pantalla
             pygame.display.flip()
+
+            x += 3
+            if x >= 640-64:
+                hayGanador = True
+
+        pygame.quit()
+        sys.exit()
 
 
 if __name__ == '__main__':
     pygame.init()
     game = Game(640, 480)
     game.competir()
+
+# 2330
